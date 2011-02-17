607e7d011ed6d40e7734fb3875e92f494f2b717c
dante.gpap@gmail.com <dante.gpap@gmail.com@82ef593e-3072-98b3-0959-d2d13de6ca92>
2011-02-17 18:41:03 +0000

    added 2 new files first_sync.cpp//first_sync.h modified objects.h file

---
+++ mirror/first_sync.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   first_sync.h
+ * Author: dante
+ *
+ * Created on February 17, 2011, 7:57 PM
+ */
+
+#ifndef FIRST_SYNC_H
+#define	FIRST_SYNC_H
+
+#include <iostream>
+#include <cstdlib>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "objects.h"
+
+using namespace std;
+
+#endif	/* FIRST_SYNC_H */
+
