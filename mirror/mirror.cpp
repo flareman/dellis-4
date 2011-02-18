f7f5d326d164bafc51abdc9494d14af48a9f0fc2
nflare@gmail.com <nflare@gmail.com@82ef593e-3072-98b3-0959-d2d13de6ca92>
2011-02-18 22:10:01 +0000

    - Added mirror.cpp/.h - Implemented basic main() function - Implemented directory validation function placeholder

---
+++ mirror/mirror.cpp
@@ -0,0 +1,35 @@
+/*
+ *  mirror.cpp
+ *  mirror
+ *
+ *  Created by Σπύρος Σμπαρούνης on 19/02/2011.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "mirror.h"
+
+using namespace std;
+
+int main (int argc, char* argv[]) {
+	if (argc < 3) {
+		cerr << "Error: syntax is mirror <source> <target>" << endl;
+		exit(1);
+	}
+	
+	if (!isDirectory(argv[1])) {
+		cerr << "Error: specified source \"" << argv[1] << "\" is not a valid directory." << endl;
+		exit(1);
+	}
+	
+	if (!isDirectory(argv[2])) {
+		cerr << "Error: specified target \"" << argv[1] << "\" is not a valid directory." << endl;
+		exit(1);
+	}
+	
+	return 0;
+}
+
+bool isDirectory (char* directoryName) {
+	return TRUE;
+}
