1f8764dc3dce1e1d25ac52a2307b8874b65ceb17
nflare@gmail.com <nflare@gmail.com@82ef593e-3072-98b3-0959-d2d13de6ca92>
2011-02-18 22:10:01 +0000

    - Added mirror.cpp/.h - Implemented basic main() function - Implemented directory validation function placeholder

---
+++ trunk/mirror/mirror.h
@@ -0,0 +1,12 @@
+/*
+ *  mirror.h
+ *  mirror
+ *
+ *  Created by Σπύρος Σμπαρούνης on 19/02/2011.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <iostream>
+
+bool isDirectory (char* directoryName);
