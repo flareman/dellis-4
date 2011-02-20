7d4835be7a3e6b1db25ee43da74458c0af4c267e
nflare@gmail.com <nflare@gmail.com@82ef593e-3072-98b3-0959-d2d13de6ca92>
2011-02-20 19:58:19 +0000

    - Implemented performSync and recursiveSync - Implemented basic main function

--- mirror/first_sync.h
+++ mirror/first_sync.h
@@ -18,6 +18,7 @@ int getdir(string dir, list<string> &files);
 directoryElement create_hierarchy(string filename, iNodeMap& nodeMap);
 directoryElement* recurse_hierarchy(string filename, string path, iNodeMap& nodeMap);
 void performInitialSync (mirrorEntity source, mirrorEntity target);
+void recursiveSync (directoryElement* source, directoryElement* target, iNodeMap& targetNodes);
 
 #endif	/* FIRST_SYNC_H */
 
