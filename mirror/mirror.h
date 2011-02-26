2e180dd83a362a7c4b440e03efbc70757c59edd2
nflare@gmail.com <nflare@gmail.com@82ef593e-3072-98b3-0959-d2d13de6ca92>
2011-02-26 23:50:30 +0000

    - Added fetchEvents, parseEventBuffer, watchForChanges member functions to notificationMonitor (event buffer filling and parsing) - Added inotify watch cleanup on notificationMonitor clear out - Added placeholder for processEvent(iNotifyEvent* theEvent) to notificationMonitor

--- mirror/mirror.h
+++ mirror/mirror.h
@@ -7,9 +7,14 @@
  *
  */
 
+#ifndef _MIRROR_H
+#define _MIRROR_H
+
 #include "generalHeaders.h"
 #include "notificationMonitor.h"
 
 using namespace std;
 
 bool isDirectory (char* directoryName);
+
+#endif
--- mirror/notificationMonitor.h
+++ mirror/notificationMonitor.h
@@ -7,8 +7,16 @@
  *
  */
 
+#ifndef _NOTIFICATION_MONITOR_H
+#define _NOTIFICATION_MONITOR_H
+
 #include "generalHeaders.h"
 #include "rw_functions.h"
+
+typedef struct inotify_event iNotifyEvent;
+
+const int eventSize = sizeof(iNotifyEvent);
+const int eventBufferSize = 1024*(eventSize+32);
 
 using namespace std;
 
@@ -17,17 +25,29 @@ private:
 	map<int,directoryElement*> assignments;
 	mirrorEntity source, target;
 	int notificationSocket;
+	char eventBuffer[eventBufferSize];
+	int currentPosition;
+	
 	int getdir(string dir, list<string>& files);
 	directoryElement* recurse_hierarchy(string filename, string path, iNodeMap& nodeMap);
 	void recursiveWatch(directoryElement* theElement);
 	void recursiveSync(directoryElement* source, directoryElement* target, iNodeMap& targetNodes);
+	int fetchEvents();
+	bool parseEventBuffer();
+	void processEvent(iNotifyEvent* theEvent);
 public:
 	void performInitialSync();
 	void clearMonitor();
-	notificationMonitor() { source.root = NULL; target.root = NULL; };
+	notificationMonitor() {
+		source.root = NULL; target.root = NULL;
+		memset(&eventBuffer, 0, eventBufferSize); currentPosition = 0;
+	};
 	~notificationMonitor() { clearMonitor(); };
 	bool initializeMonitor(char* sourcePath, char* targetPath);
 	void assignWatches();
+	void watchForChanges();
 };
 
 bool compareDirectories (directoryElement* first, directoryElement* second);
+
+#endif
