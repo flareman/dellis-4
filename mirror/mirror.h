5249b1ecc76ce13760244c14fe23c163fe75b196
nflare@gmail.com <nflare@gmail.com@82ef593e-3072-98b3-0959-d2d13de6ca92>
2011-03-03 23:40:15 +0000

    - Removed watchedEvents clause. - Removed automatically generated creator comments at beginning of files.

--- mirror/mirror.h
+++ mirror/mirror.h
@@ -1,12 +1,3 @@
-/*
- *  mirror.h
- *  mirror
- *
- *  Created by Σπύρος Σμπαρούνης on 19/02/2011.
- *  Copyright 2011 __MyCompanyName__. All rights reserved.
- *
- */
-
 #ifndef _MIRROR_H
 #define _MIRROR_H
 
